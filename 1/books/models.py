01d726f92caaaac0cfdc56228365969b0a6ca938
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-06-15 15:02:59 +0330

    Django-Practise:1:LibraryManagementSystem: Author and Member models created in books.models

--- 1/books/models.py
+++ 1/books/models.py
@@ -12,3 +12,11 @@ class Book(models.Model):
     ISBN = models.BigIntegerField()
     availability_status = models.BooleanField(default=True)
     
+class Author:
+    name = models.CharField(max_length=200)
+    biography = models.TextField()
+    
+class Memeber:
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    contact_number = models.BigIntegerField()
