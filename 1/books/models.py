41afdc47157479c5460de210c7f1a8246a7f3042
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-06-15 16:11:12 +0330

    Django-Practise:1:LibraryManagementSystem: model relations handled in books.models

--- 1/books/models.py
+++ 1/books/models.py
@@ -1,22 +1,25 @@
 from django.db import models
-from django.contrib.auth.models import User, AnonymousUser
 
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=150)
-    author = models.ForeignKey(to=User,
-                               on_delete=models.SET_DEFAULT,
-                               default=AnonymousUser
-                               )
+    author = models.ManyToManyField("Author")
     publication_date = models.DateField(null=True)
     ISBN = models.BigIntegerField()
     availability_status = models.BooleanField(default=True)
     
-class Author:
+class BorrowModel(models.Model):
+    member = models.ForeignKey("Memeber")
+    book = models.ForeignKey("Book")
+    borrow_date = models.DateField()
+    return_date = models.DateField()
+    
+class Author(models.Model):
     name = models.CharField(max_length=200)
     biography = models.TextField()
     
-class Memeber:
+class Memeber(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField()
     contact_number = models.BigIntegerField()
+    
