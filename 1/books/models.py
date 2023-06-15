df1514447a9587967897f1a24d017705053aedf9
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-06-15 14:58:21 +0330

    Django-Practise:1:LibraryManagementSystem: Book model created in books.models

--- 1/books/models.py
+++ 1/books/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User, AnonymousUser
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=150)
+    author = models.ForeignKey(to=User,
+                               on_delete=models.SET_DEFAULT,
+                               default=AnonymousUser
+                               )
+    publication_date = models.DateField(null=True)
+    ISBN = models.BigIntegerField()
+    availability_status = models.BooleanField(default=True)
+    
