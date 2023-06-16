18ae7f5595b0528993ba4ce29be5259ba7ad3245
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-06-16 12:49:24 +0330

    DjangoPractise:2:Calculator: add phase comleted. route and url added

--- 2/calculator_app/urls.py
+++ 2/calculator_app/urls.py
@@ -3,4 +3,5 @@ from .views import add_page, home
 
 urlpatterns = [
     path('', home, name='homepage'),
+    path('add/', add_page, name='Addition'),
 ]
--- 2/calculator_app/views.py
+++ 2/calculator_app/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def home(request):
     if request.method == 'POST':
-        print(request.POST.get('op'))
-    
+        return redirect(request.POST.get('op'))
     return render(request, 'calculator_app/home.html', {})
+
+def add_page(request):
+    if request.method == 'POST':
+        result = int(request.POST.get('num1')) + int(request.POST.get('num2'))
+        context = {
+            'op': '+',
+            'result': result,
+            'num1': request.POST.get('num1'),
+            'num2': request.POST.get('num2')
+        }
+        return render(request, 'calculator_app/calc.html', context)
+    return render(request, 'calculator_app/calc.html', {})   
