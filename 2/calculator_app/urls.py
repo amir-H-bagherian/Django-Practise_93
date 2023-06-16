07c3fffcab6445db6c6386749c46118777c1c93c
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-06-16 12:54:37 +0330

    DjangoPractise:2:Calculator: subtraction, multiplication & division phases comleted. route and url added

--- 2/calculator_app/urls.py
+++ 2/calculator_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import addition_page, home
+from .views import home, addition_page, subtraction_page, multiplication_page, division_page
+
 
 urlpatterns = [
     path('', home, name='homepage'),
     path('add/', addition_page, name='Addition'),
+    path('sub/', subtraction_page, name='Subtraction'),
+    path('mul/', multiplication_page, name='Multiplication'),
+    path('div/', division_page, name='Division'),
 ]
--- 2/calculator_app/views.py
+++ 2/calculator_app/views.py
@@ -16,4 +16,40 @@ def addition_page(request):
             'num2': request.POST.get('num2')
         }
         return render(request, 'calculator_app/calc.html', context)
-    return render(request, 'calculator_app/calc.html', {})   
+    return render(request, 'calculator_app/calc.html', {})  
+ 
+def multiplication_page(request):
+    if request.method == 'POST':
+        result = int(request.POST.get('num1')) * int(request.POST.get('num2'))
+        context = {
+            'op': '*',
+            'result': result,
+            'num1': request.POST.get('num1'),
+            'num2': request.POST.get('num2')
+        }
+        return render(request, 'calculator_app/calc.html', context)
+    return render(request, 'calculator_app/calc.html', {})  
+ 
+def division_page(request):
+    if request.method == 'POST':
+        result = int(request.POST.get('num1')) / int(request.POST.get('num2'))
+        context = {
+            'op': '/',
+            'result': result,
+            'num1': request.POST.get('num1'),
+            'num2': request.POST.get('num2')
+        }
+        return render(request, 'calculator_app/calc.html', context)
+    return render(request, 'calculator_app/calc.html', {})  
+ 
+def subtraction_page(request):
+    if request.method == 'POST':
+        result = int(request.POST.get('num1')) - int(request.POST.get('num2'))
+        context = {
+            'op': '-',
+            'result': result,
+            'num1': request.POST.get('num1'),
+            'num2': request.POST.get('num2')
+        }
+        return render(request, 'calculator_app/calc.html', context)
+    return render(request, 'calculator_app/calc.html', {})  
