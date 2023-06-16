7482bb5f23fedb8d996c272e4323957dcd36bd70
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-06-16 15:10:57 +0330

    DjangoPractise:3:TextToHTMLConverter: route & url added for text-to-html conversion

--- 3/converter_app/urls.py
+++ 3/converter_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import converter_view
+from .views import converter_view, create_result
 
 
 urlpatterns = [
-    path('', converter_view, name='home')
+    path('', converter_view, name='home'),
+    path('create-result/', create_result, name='result'),
 ]
--- 3/converter_app/views.py
+++ 3/converter_app/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 # Create your views here.
 def converter_view(request):
     return render(request, 'converter_app/converter.html', {})
+
+def create_result(request):
+    if request.method == 'POST':
+        text = request.POST.get('html-text')
+        return HttpResponse(text)
+    else:
+        return redirect('home')
