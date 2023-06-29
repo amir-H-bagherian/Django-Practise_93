ab900c749346ebe69f796891bca81b20934db9db
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-06-29 17:14:45 +0330

    DjangoPractise:4:ToDoList: CreateToDoView added to views.py

--- 4/todoapp/views.py
+++ 4/todoapp/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .models import ToDoItem
+from .forms import ToDoCreateForm
+from django.contrib import messages
 # Create your views here.
 
 class HomeToDoListView(View):
@@ -11,3 +13,19 @@ class HomeToDoListView(View):
             'todos': todos
         }
         return render(request, 'todoapp/home.html', context)
+    
+class CreateToDoView(View):
+    
+    form_class = ToDoCreateForm()
+    
+    def get(self, request):
+        my_form = self.form_class()
+        return render(request, 'todoapp/todo-create.html', {'form': my_form})
+    
+    def post(self, request):
+        my_form = self.form_class(request.POST)
+        if my_form.is_valid():
+            new_todo = my_form.save()
+            messages.add_message(request, messages.SUCCESS, f"{new_todo.title} task created!")
+            return redirect('home')
+        return render(request, 'todoapp/todo-create.html', {'form': my_form})
