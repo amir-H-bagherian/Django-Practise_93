74cbf318c5de8a1256c298c85ae4c4f74ba932ab
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-06-29 17:15:28 +0330

    DjangoPractise:4:ToDoList: CreateToDoView route added to urls.py

--- 4/todoapp/urls.py
+++ 4/todoapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomeToDoListView
+from .views import HomeToDoListView, CreateToDoView
 
 urlpatterns = [
     path('', HomeToDoListView.as_view(), name='home'),
+    path('create/', CreateToDoView.as_view(), name='create-todo'),
 ]
