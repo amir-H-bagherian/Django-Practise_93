6722310741deff248fe4e08007e6d343afa20dfb
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-06-29 16:10:55 +0330

    DjangoPractise:4:ToDoList: HomeToDoListView class  added to views.py

--- 4/todoapp/views.py
+++ 4/todoapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.views import View
+from .models import ToDoItem
 # Create your views here.
+
+class HomeToDoListView(View):
+    
+    def get(self, request):
+        todos = ToDoItem.objects.all()
+        context = {
+            'todos': todos
+        }
+        return render(request, 'home.html', context)
