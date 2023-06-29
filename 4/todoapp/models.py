af9b5a0c05d50d22cb2b46f81fb4cb3d87561291
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-06-29 16:04:09 +0330

    DjangoPractise:4:ToDoList: ToDoItem model is added

--- 4/todoapp/models.py
+++ 4/todoapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class ToDoItem (models.Model):
+
+    title = models.CharField(max_length=100)
+    desc = models.TextField()
+    
+    class Meta:
+        verbose_name = _("To do Item")
+        verbose_name_plural = _("To do Items")
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("todo_detail", kwargs={"pk": self.pk})
