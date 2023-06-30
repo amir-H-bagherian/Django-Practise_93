bd4b7a63870d10b98ea0a2c5b575b329515bbbd4
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-06-30 15:02:56 +0330

    DjangoPractise:5:CustomAuth: CustomUser model added to models.py

--- 5/custom_auth/models.py
+++ 5/custom_auth/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from .manager import CustomUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, AbstractUser, User
+from django.core.validators import RegexValidator
 
-# Create your models here.
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    
+    phone_regex = RegexValidator(
+        regex=r'^\+?1?\d{9,15}$',
+        message="Please enter a valid phone number!"
+    )
+    phone_number = models.CharField(max_length=16, verbose_name="User Phone Number",
+                                    validators=phone_regex, unique=True)
+    email = models.EmailField()
+    
+    objects = CustomUserManager()
+    
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = ['email']
+    
+    class Meta:
+        verbose_name = 'Custom User'
+        verbose_name_plural = 'Custom Users'
+        
+    def __str__(self) -> str:
+        return self.email
+        
