517dfa559bed5bf18d95c0ef4036dbfc09cb2abe
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-08-10 10:41:59 +0330

    fix: PostManager added to Post model in app.models

--- 7/app/models.py
+++ 7/app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from core.models import CustomUser, BaseModel
-
+from .managers import PostManager
 
 class Post(BaseModel):
     title = models.CharField(max_length=100)
@@ -8,6 +8,9 @@ class Post(BaseModel):
     author = models.ForeignKey(to=CustomUser,
                                on_delete=models.SET_NULL,
                                null=True)
+    
+    objects = PostManager()
+    
     
 class Comment(BaseModel):
     post = models.ForeignKey(to=Post,
