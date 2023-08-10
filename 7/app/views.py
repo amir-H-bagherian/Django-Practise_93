61bc044076f4777b8a5499699aa9befe1f6a32eb
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-08-10 11:01:34 +0330

    feat: PostView and PostDetailView classes added to app.views

--- 7/app/views.py
+++ 7/app/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework.views import APIView, Response, status
 
-# Create your views here.
+from .models import Comment, Post
+from .serializers import PostSerializer, CommentSerializer
+
+
+class PostView(APIView):
+    
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(instance=posts, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        serializer = PostSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+    
+    
+class PostDetailView(APIView):
+    
+    def get(self, request, pk):
+        post =  get_object_or_404(Post, pk=pk)
+        serializer = PostSerializer(instance=post)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+    
+    def put(self, request, pk):
+        post =  get_object_or_404(Post, pk=pk)
+        serializer = PostSerializer(instance=post, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+    
+    def delete(self, request, pk):
+        post =  get_object_or_404(Post, pk=pk)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
