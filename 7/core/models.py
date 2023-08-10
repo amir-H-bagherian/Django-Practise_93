6035b3241123c598d6a2c1adf4938c2b4c2f9259
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-08-10 10:08:12 +0330

    feat: BaseModel and CustomUser added to core.models

--- 7/core/models.py
+++ 7/core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from .managers import CustomUserManager
 
 # Create your models here.
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+    
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField("email address", unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
