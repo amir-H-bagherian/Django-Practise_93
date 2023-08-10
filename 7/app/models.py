e7a4451ff36c1486e8a9c510c55ca1243821c3eb
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-08-10 10:15:51 +0330

    feat: Post and Comment models added to app.models file

--- 7/app/models.py
+++ 7/app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from core.models import CustomUser, BaseModel
 
-# Create your models here.
+
+class Post(BaseModel):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(to=CustomUser,
+                               on_delete=models.SET_NULL,
+                               null=True)
+    
+class Comment(BaseModel):
+    post = models.ForeignKey(to=Post,
+                             on_delete=models.CASCADE)
+    content = models.TextField()
