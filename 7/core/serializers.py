621feaa4f13bfbcc810e330b936147a2280ae6b1
Amir Hossein Bagherian <amir.h.bagherian.k2020@gmail.com>
2023-08-11 20:01:21 +0330

    fix: validate_password method added to CustomUserSerializer in core.serializers

--- 7/core/serializers.py
+++ 7/core/serializers.py
@@ -1,9 +1,22 @@
 from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
 from .models import CustomUser
-
 
 class CustomUserSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = CustomUser
         fields = '__all__'
+        
+    def validate_password(self, value: str) -> str:
+        """
+        Hash value passed by user.
+
+        :param value: password of a user
+        :return: a hashed version of the password
+        """
+        return make_password(value)
+    
+# class LoginSerializer(serializers.Serializer):
+#     email = serializers.EmailField()
+#     password = serializers.CharField()
